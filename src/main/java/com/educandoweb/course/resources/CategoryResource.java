9b308eecdaa780731c83b0e14e3e9be1e38c09d5
Helio Dourado <heliohdd@yahoo.com.br>
2019-12-05 15:18:32 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/resources/CategoryResource.java
+++ src/main/java/com/educandoweb/course/resources/CategoryResource.java
@@ -22,7 +22,7 @@ public class CategoryResource {
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll() {
 		List<Category> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+ 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
